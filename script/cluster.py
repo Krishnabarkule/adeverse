14b119a29963bcc8618dabacbc5d2076c942e305
Krishna Barkule <krishnabarkule@gmail.com>
2024-03-03 23:07:47 +0530

    update

--- script/cluster.py
+++ script/cluster.py
@@ -18,13 +18,13 @@ def start_cluster(cluster_id:str,db_host:str,db_token:str):
         print(e)
 
 
-DB_url = "https://agilisium.cloud.databricks.com"
-DB_token = "dapic961af96fc16cf8e8c58618c5aeee123"
-cluster_id = "0828-132635-71kqd24v"
+# DB_url = "https://agilisium.cloud.databricks.com"
+# DB_token = "dapic961af96fc16cf8e8c58618c5aeee123"
+# cluster_id = "0828-132635-71kqd24v"
 
-res=start_cluster(
-    cluster_id=cluster_id,
-    db_host=DB_url,
-    db_token=DB_token
-)
-print(res)
+# res=start_cluster(
+#     cluster_id=cluster_id,
+#     db_host=DB_url,
+#     db_token=DB_token
+# )
+# print(res)
