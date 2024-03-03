dbe3696889dcddac9624f0ab1a5a740a142140d7
Krishna Barkule <krishnabarkule@gmail.com>
2024-03-03 23:14:05 +0530

    s

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import configparser
-from script.cluster import start_cluster
+# from script.cluster import start_cluster
 from script.local_to_s3 import copy_all_files_to_s3
 from script.unpro_to_pro import copy_files_within_bucket, delete_files_in_folder
 #from script.milvus import read_json_data_from_s3,create_embedding_dataframe, create_milvus_collection
@@ -37,7 +37,7 @@ def main():
     cluster_id = parameters['cluster_id']
 
     try:
-        start_cluster(cluster_id=cluster_id,db_host=db_host,db_token=db_token)
+        # start_cluster(cluster_id=cluster_id,db_host=db_host,db_token=db_token)
         copy_all_files_to_s3(local_folder, bucket_name, s3_source_folder, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token)
         copy_files_within_bucket(bucket_name, s3_source_folder, s3_target_folder, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token)
         # combined_df = read_json_data_from_s3(bucket_name, folder_prefix=s3_source_folder, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token)
